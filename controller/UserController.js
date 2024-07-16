545c4400d61723fd4f9713cf9d719b1922608dfe
juanpablovaz <juanpablodealmeidavaz2002@gmail.com>
2024-07-15 22:20:40 -0300

    Atualizei dia 15-07

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,8 +1,59 @@
 class UserController {
 
-    constructor(formId) {
+    constructor(formId,tableId) {
 
         this.formEl = document.getElementById(formId);
+        this.tableEl = document.getElementById(tableId);
+        
+        this.onSubmit();
+
+    }
+
+    onSubmit() {
+
+
+        this.formEl.addEventListener("submit",  event => {
+
+            event.preventDefault();
+
+            let values = this.getValues();
+
+            
+
+            this.getPhoto((content)=>{
+                values.photo = content;
+
+                this.addLine(values);
+            });
+
+            
+        });
+
+    }
+
+    getPhoto(callback){
+
+        let fileReader = new FileReader();
+
+        let elements = [...this.formEl.elements].filter(item =>{
+            if (item.name == 'photo'){
+                return item;
+            }
+            
+        })
+
+       let file = (elements[0].files[0]);
+
+        fileReader.onload = () =>{
+
+            //ISSO É UMA FUNÇÃO CALLBACK, QUE É UMA FUNÇÃO SDE RETORNO.   
+
+            
+
+            callback(fileReader.result);
+        }
+
+        fileReader.readAsDataURL(file);
 
     }
 
@@ -10,7 +61,9 @@ class UserController {
 
         let user = {};
 
-        this.formEl.nextElements.forEach(function (field, index) {
+        //this.formEl.elements[0], this.formEl.elements[1]......
+
+    [...this.formEl.elements].forEach(function (field, index) {
 
             if (field.name == "gender") {
 
@@ -28,7 +81,7 @@ class UserController {
         });
 
 
-        var objectUser = new User(
+        return new User(
             user.name,
             user.gender,
             user.birth,
@@ -41,4 +94,26 @@ class UserController {
 
         return objectUser;
     }
+
+
+    addLine(dataUser) {
+
+        this.tableEl.innerHTML = `
+            <tr>
+                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+                <td>${dataUser.name}</td>
+                <td>${dataUser.email}</td>
+                <td>${dataUser.admin}</td>
+                <td>${dataUser.birth}</td>
+                <td>
+                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td>
+            </tr>
+        `;
+    
+    }
+
+
+
 }
