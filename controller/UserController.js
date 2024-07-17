d9bdc49c41a31a67f165ca9b7e9b79eed20f3c50
juanpablovaz <juanpablodealmeidavaz2002@gmail.com>
2024-07-16 23:00:33 -0300

    Atualizei

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,10 +1,10 @@
 class UserController {
 
-    constructor(formId,tableId) {
+    constructor(formId, tableId) {
 
         this.formEl = document.getElementById(formId);
         this.tableEl = document.getElementById(tableId);
-        
+
         this.onSubmit();
 
     }
@@ -12,48 +12,67 @@ class UserController {
     onSubmit() {
 
 
-        this.formEl.addEventListener("submit",  event => {
+        this.formEl.addEventListener("submit", event => {
 
             event.preventDefault();
 
+            let btn = this.formEl.querySelector('[type=submit]');
+
+            btn.disable = true;
+
             let values = this.getValues();
 
-            
 
-            this.getPhoto((content)=>{
-                values.photo = content;
+            this.getPhoto().then(
+                (content) =>{
+                    values.photo = content;
 
                 this.addLine(values);
-            });
 
-            
+                this.formEl.reset();
+                
+                btn.disable = false
+                },
+                (e)=>{
+                    console.error(e)
+                }
+            )
         });
 
     }
 
-    getPhoto(callback){
+    getPhoto() {
 
-        let fileReader = new FileReader();
+        return new Promise((resolve, reject) =>{
 
-        let elements = [...this.formEl.elements].filter(item =>{
-            if (item.name == 'photo'){
-                return item;
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+                if (item.name == 'photo') {
+                    return item;
+                }
+
+            })
+
+            let file = (elements[0].files[0]);
+
+            fileReader.onload = () => {
+
+                //ISSO É UMA FUNÇÃO CALLBACK, QUE É UMA FUNÇÃO DE RETORNO.   
+
+                resolve(fileReader.result);
             }
-            
-        })
 
-       let file = (elements[0].files[0]);
+            fileReader.onerror = (e)=>{
+                reject(e);
+            }
 
-        fileReader.onload = () =>{
-
-            //ISSO É UMA FUNÇÃO CALLBACK, QUE É UMA FUNÇÃO SDE RETORNO.   
-
-            
-
-            callback(fileReader.result);
-        }
-
-        fileReader.readAsDataURL(file);
+            if (file){
+                fileReader.readAsDataURL(file);
+            }else{
+                resolve('dist/img/boxed-bg.jpg');
+            } 
+        });
 
     }
 
@@ -63,7 +82,7 @@ class UserController {
 
         //this.formEl.elements[0], this.formEl.elements[1]......
 
-    [...this.formEl.elements].forEach(function (field, index) {
+        [...this.formEl.elements].forEach(function (field, index) {
 
             if (field.name == "gender") {
 
@@ -73,6 +92,9 @@ class UserController {
 
                 }
 
+            }else if(field.name == "admin"){
+
+                user[field.name] = field.checked;
             } else {
 
                 user[field.name] = field.value;
@@ -98,20 +120,25 @@ class UserController {
 
     addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
-            <tr>
+        let tr = document.createElement('tr');
+        
+
+        tr.innerHTML = `
+            
                 <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
+                <td>${(dataUser.admin) ? 'sim' : 'nao'}</td>
+                <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
                     <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
-            </tr>
+            
         `;
-    
+
+        this.tableEl.appendChild(tr);
+
     }
 
 
